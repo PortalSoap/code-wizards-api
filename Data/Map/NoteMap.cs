49223f76637e6dd9f42a344a89bf198beb0cef94
Lucas M. Lacerda <82623575+PortalSoap@users.noreply.github.com>
2023-09-25 01:01:07 -0300

    Nova migração criada para impelementar vínculo.
    Uma nova migração foi adicionada ao banco de dados para implementar o sistema de vínculo com o usuário.
    OBS: O mapeamento também foi atualizado para este fim.

--- Data/Map/NoteMap.cs
+++ Data/Map/NoteMap.cs
@@ -11,6 +11,8 @@ namespace code_wizards_api.Data.Map
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Title).IsRequired().HasMaxLength(255);
             builder.Property(x => x.Description).HasMaxLength(1000);
+            builder.Property(x => x.UserId);
+            builder.HasOne(x => x.User);
         }
     }
 }
---
+++ Migrations/20230925035142_NoteLinking.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace code_wizards_api.Migrations
+{
+    public partial class NoteLinking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
